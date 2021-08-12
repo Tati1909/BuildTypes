7f063dbb073e144a3673242566d156ae2abc94bc
татьяна <emuart155@gmail.com>
2021-08-12 15:37:58 +0300

    Версия debug

--- app/src/main/java/com/example/buildtypes/MainActivity.kt
+++ app/src/main/java/com/example/buildtypes/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.buildtypes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.buildtypes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.textView.text = BuildConfig.TYPE
     }
 }
